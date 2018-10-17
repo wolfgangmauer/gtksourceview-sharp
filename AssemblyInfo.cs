e388391a76782eb38d09a62aa2e3f1cd91ab0ce3
Wolfgang Mauer <wolfgang.mauer@kabelmail.de>
2018-10-18 00:20:07 +0200

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly:AssemblyVersion("1.0.0.2")]
+[assembly:AssemblyDelaySign(false)]
+[assembly:AssemblyKeyFile("gtk-sharp.snk")]
